ae893784b8c65600d5d27d5179e910a08c9379fe
Niephox <rezutasos@gmail.com>
2018-12-31 01:24:35 +0200

    Delete calculateDistance.js

--- calculateDistance.js
+++
@@ -1,9 +0,0 @@
-var distance = require('google-distance-matrix');
-distance.key('AIzaSyBVYdskL-346Vho2AbcbJxQNUA0s0pjiSI');
-var origins = ['San Francisco CA'];
-var destinations = ['New York NY', '41.8337329,-87.7321554'];
-
-distance.matrix(origins, destinations, function (err, distances) {
-    if (!err)
-        console.log(distances);
-})
